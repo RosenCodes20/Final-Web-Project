f4b32ca4fe0f1dfc674493a10da63f1564835327
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-12-08 20:44:28 +0200

    feat: upgrated settings.

--- football_team_manager/settings.py
+++ football_team_manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', config('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', config('DEBUG')) == "True"
+DEBUG = os.getenv('DEBUG', config('DEBUG', default=False)) == "True"
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
 
