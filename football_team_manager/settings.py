224d720c9a13954acb251aec89204fdff9f0a018
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-12-08 20:53:10 +0200

    feat: upgrated settings.

--- football_team_manager/settings.py
+++ football_team_manager/settings.py
@@ -112,9 +112,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv('DB_NAME', config('DB_NAME')),
         "USER": os.getenv('DB_USER', config('DB_USER')),
-        "PASSWORD": os.getenv('DB_PASS', config('PASS')),
-        "HOST": os.getenv('DB_HOST', config('HOST')),
-        "PORT": os.getenv('DB_PORT', config('PORT')),
+        "PASSWORD": os.getenv('DB_PASS', config('DB_PASS')),
+        "HOST": os.getenv('DB_HOST', config('DB_HOST')),
+        "PORT": os.getenv('DB_PORT', config('DB_PORT')),
     }
 }
 
