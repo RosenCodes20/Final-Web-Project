8d7b1a8f9fbdcc4cd8805b536d3b57b3273d81eb
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-12-08 16:10:49 +0200

    feat: hide the env.

--- football_team_manager/settings.py
+++ football_team_manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG") == True
+DEBUG = config("DEBUG") == "True"
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
