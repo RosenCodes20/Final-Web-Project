b3d4e1c00b3e37a31d3ba9a6316c05eb43c5f089
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-12-10 19:30:00 +0200

    feat: added new migrations.

--- football_team_manager/players/admin.py
+++ football_team_manager/players/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from football_team_manager.players.models import Player
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_filter = ['name']
+    ordering = ['age', 'name']
+    list_display = ['name', 'age', 'club']
+    search_fields = ['name', 'club__team_name']
+    readonly_fields = ['user', 'club']
