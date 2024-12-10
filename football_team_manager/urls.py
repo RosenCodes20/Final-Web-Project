473aedaa1940dc2804c40b7234cb551e974fb4be
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2024-12-10 18:11:16 +0200

    feat: updated media url, now see in production.

--- football_team_manager/urls.py
+++ football_team_manager/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("players/", include("football_team_manager.players.urls")),
@@ -26,9 +27,12 @@ urlpatterns = [
     path("leagues/", include("football_team_manager.leagues.urls")),
     path("accounts/", include("football_team_manager.accounts.urls")),
     path("schemes/", include("football_team_manager.schemes.urls")),
-    path("", include("football_team_manager.common.urls"))
+    path("", include("football_team_manager.common.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
